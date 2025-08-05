5738adadfad856019b4ca24df6225ec09d26b2f1
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-08-05 16:39:17 -0300

    🐛fix: adjust the card image and structure

--- src/components/Section/BarberCard.tsx
+++ src/components/Section/BarberCard.tsx
@@ -6,10 +6,10 @@ interface BarberProps{
 
 export default function BarberCard({title, address,image}:BarberProps){
     return (
-        <div className="w-[220.8px] h-[295px] bg-secondary_black border border-gray1 rounded-2xl pt-1 px-1 relative">
+        <div className="w-[220.8px] h-auto bg-secondary_black border border-gray1 rounded-2xl pt-1 px-1 relative">
             
            
-                <img src={image} alt={`Image da Barbearia ${Image}`} className="w-[213px] h-[159px] rounded-2xl"/>
+                <img src={image} alt={`Image da Barbearia ${title}`} className="w-auto max-h-[159px] object-cover rounded-2xl"/>
             <div className="flex flex-col p-3 gap-2">
                 <div className="h-[60px] flex flex-col gap-1 ">
                     <h2 className="font-bold text-base text-white">{title}</h2>
