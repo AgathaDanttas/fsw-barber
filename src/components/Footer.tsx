a1e57a43f2ddd38cc229b4f9cb4903f5dea3073b
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-08-05 16:16:30 -0300

    ✨feat: create component Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <footer className="w-full h-auto pt-[100px]">
+            <div className="bg-secondary_black py-6 px-5">
+                <span className="text-gray3 font-bold text-xs font-sans">© 2025 Copyright FSW Barber <span >- Agatha Dantas</span></span>
+            </div>
+        </footer>
+    )
+}
