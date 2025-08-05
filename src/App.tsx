f5b9968569a3120cbf971526fadd9a926b7866dd
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-08-05 16:15:19 -0300

    ✨feat(app): layout structure with section, footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import './App.css'
 import './index.css'
 import Header from './components/header'
 import Frame from './components/Frame'
+import Section from './components/Section/Section'
+import BarberCard
+  from './components/Section/BarberCard'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -11,6 +15,64 @@ function App() {
       <Header />
       <hr className='border-gray1 border '/>
       <Frame />
+      <Section
+        section_title='Populares'>
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+        
+        </Section>
+      <Section
+        section_title='Mais Visitados'>
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+         <BarberCard
+              image='/assets/barber1.jpg'
+              title='Vintage Barber' 
+              address='Avenida São Sebastião, 357, São Paulo'
+                       />
+        </Section>
+       <Footer/>
     </>
   )
 }
