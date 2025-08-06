5e26237d73f688c144b5463b154c4bb907aef6be
Agatha Dantas <workdantas@gmail.com>
2025-08-05 23:23:12 -0300

    💄style: create and styles button

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,25 +1,35 @@
 import type { ReactNode } from "react";
+import next from '../../assets/icons/next.png'
 
-interface SectionProps{
+interface SectionProps {
     section_title: string;
     children: ReactNode;
 }
 
 
-export default function Section({section_title, children}: SectionProps) {
+export default function Section({ section_title, children }: SectionProps) {
     return (
         <div className="w-full min-w-[600px] h-auto pt-10 px-32 flex flex-col gap-5 items-center">
 
             <div className="w-auto flex flex-col gap-5">
 
-            <span className="font-sans font-bold text-xl text-white ">{section_title}</span>
+                <span className="font-sans font-bold text-xl text-white ">{section_title}</span>
 
 
-  
-            <div className="flex h-[295px] gap-5 justify-center">
-                { children}
+
+                <div className="flex h-[295px] justify-center relative">
+                    <div className="flex gap-5 ">
+                         {children}
+                    </div>
+                   
+                   <div className="flex items-center  absolute bottom-0  right-[-23px] top-0 
+                      ">
+                     <button className="bg-background_black border w-[55px] h-[55px] rounded-full border-[2px] border-gray1 flex items-center justify-center ">
+                        <img src={next} alt="Next" className="w-[40px] h-[40px]" />
+                    </button>
+                   </div>
+                </div>
             </div>
-            </div>   
         </div>
     );
 }
